7f7416d734d3d233b787a2ef5875bb7db13eb1e0
BaselessFabric <alexanderwalls92@gmail.com>
2023-05-08 19:30:54 +0100

    commented out certain fields in ItemDescription to pass linting

--- src/scenes/itemDetails/ItemDetails.jsx
+++ src/scenes/itemDetails/ItemDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { IconButton, Box, Typography, Button, Tabs, Tab } from "@mui/material";
+import { IconButton, Box, Typography, Button } from "@mui/material";
 // import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
@@ -17,14 +17,14 @@ function sanitizeHtml(html) {
 const ItemDetails = () => {
     const dispatch = useDispatch();
     const { itemId } = useParams();
-    const [value, setValue] = useState("description");
+    // const [value, setValue] = useState("description");
     const [count, setCount] = useState(1);
     const [item, setItem] = useState(null);
     const [items, setItems] = useState([]);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+    // const handleChange = (event, newValue) => {
+    //     setValue(newValue);
+    // };
 
     async function getItem() {
         const item = await fetch(
